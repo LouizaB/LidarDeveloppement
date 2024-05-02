85292ba3972a622479fece35e67018fe028d1171
BENAMEUR Kaoutar <66181357+Kaoutar-ben@users.noreply.github.com>
2024-05-02 18:41:52 +0200

    Add files via upload

--- chm-dtm-dsm.R
+++ chm-dtm-dsm.R
@@ -1,8 +1,7 @@
 library(shiny)
-library(lidR)
-library(sf)
+library(terra)
 
-# Définition de l'interface utilisateur
+# Define UI
 ui <- fluidPage(
   titlePanel("Visualisation des images LiDAR"),
   
@@ -20,32 +19,30 @@ ui <- fluidPage(
   )
 )
 
-# Définition du serveur
-server <- function(input, output) {
+# Define server logic
+server <- function(input, output, session) {
+  
   observeEvent(input$show, {
-    req(input$file)
+    inFile <- input$file
+    if (is.null(inFile)) {
+      return(NULL)
+    }
+    las <- readLAS(inFile$datapath)
     
-    # Charger le fichier LAS
-    las <- readLAS(input$file$datapath)
+    # Compute DTM, DSM, and CHM
+    dtm <- rasterize_terrain(las, output_raster, tin())
+    dsm <- rasterize_canopy(las, output_raster, p2r())
+    chm <- dsm - dtm
     
-    # Obtenir le CRS du fichier LAS
-    las_crs <- st_crs(las)
-    
-    # Définir le CRS du fichier LAS
-    st_crs(las) <- st_crs(output_raster)
-    
-    # Appliquer la technique de filtrage choisie
-    output_image <- switch(input$filter,
-                           "dsm" = plot(rasterize_canopy(las, output_raster, p2r())),
-                           "dtm" = plot(rasterize_terrain(las, output_raster, tin())),
-                           "chm" = plot(rasterize_canopy(las, output_raster, p2r()) - rasterize_terrain(las, output_raster, tin()))
-    )
-    
+    # Plot selected image
     output$image <- renderPlot({
-      output_image
+      switch(input$filter,
+             "chm" = plot(chm, main = "Canopy Height Model"),
+             "dtm" = plot(dtm, main = "Digital Terrain Model"),
+             "dsm" = plot(dsm, main = "Digital Surface Model"))
     })
   })
 }
 
-# Exécuter l'application Shiny
+# Run the application
 shinyApp(ui = ui, server = server)
