881f6c7999d37a2d21797a00ebf67efcca1604c5
yasminehamadene <120636233+yasminehamadene@users.noreply.github.com>
2024-05-04 00:13:12 +0200

    Update Update_Interface.R

--- Update_Interface.R
+++ Update_Interface.R
@@ -86,9 +86,22 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                            )
                   ),
                   tabPanel("Normalisation",
-                           mainPanel(
-                             
-                           )
+                  titlePanel("LAS Decimation Tool"),
+                  sidebarLayout(
+                    sidebarPanel(
+                      fileInput("lasfile", "Upload LAS File (.laz)"),
+                      selectInput("algorithm", "Select Algorithm", 
+                                  choices = c("random", "homogenize", "highest", "lowest", "random_per_voxel")),
+                      numericInput("param1", "Parameter 1", value = 1),
+                      numericInput("param2", "Parameter 2", value = 5),
+                      actionButton("submit", "Submit")
+                    ),
+                    mainPanel(
+                      tabsetPanel(
+                        tabPanel("Original LAS", plotOutput("original_plot")),
+                        tabPanel("Decimated LAS", plotOutput("decimated_plot"))
+                      )
+                    )
                   ),
                   
                   
@@ -97,9 +110,9 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                              # Use imageOutput to place the image on the page
                              imageOutput("test")
                            )
-                  ),
+                  )
                   
-                )
+                ))
 )
 
 # Define server function  
@@ -333,6 +346,33 @@ server <- function(input, output, session) {
         result <- dsm(las_chablais3)
       }
     }
+  })
+  observeEvent(input$submit, {
+    req(input$lasfile)
+    
+    algorithm <- switch(input$algorithm,
+                        "random" = random(input$param1),
+                        "homogenize" = homogenize(input$param1, input$param2),
+                        "highest" = highest(input$param1),
+                        "lowest" = lowest(input$param1),
+                        "random_per_voxel" = random_per_voxel(input$param1)
+    )
+    
+    # Read LAS file
+    las <- readLAS(input$lasfile$datapath, select = "xyz")
+    
+    # Decimate LAS
+    decimated_las <- decimate_points(las, algorithm)
+    
+    # Plot original LAS
+    output$original_plot <- renderPlot({
+      plot(las)
+    })
+    
+    # Plot decimated LAS
+    output$decimated_plot <- renderPlot({
+      plot(decimated_las)
+    })
   })
 }
 
